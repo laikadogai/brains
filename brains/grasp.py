8ee36222fbf62f27968a30251eb9d11cb7dba1dc
Alexander Smirnov <alex@asmirnov.xyz>
2024-05-03 23:15:23 +0100

    fix grasp script

--- brains/grasp.py
+++ brains/grasp.py
@@ -1,9 +1,9 @@
 import sys
 import time
-from os.path import join
 
 import numpy as np
-from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
+from interbotix_xs_modules.xs_robot.arm import InterbotixGripperXSInterface, InterbotixManipulatorXS
+from loguru import logger
 
 """
 This script makes the end-effector perform pick, pour, and place tasks.
@@ -13,25 +13,24 @@ Make sure to adjust commanded joint positions and poses as necessary.
 To get started, open a terminal and type:
 
     ros2 launch interbotix_xsarm_control xsarm_control.launch.py robot_model:=wx250
-
-Then change to this directory and type:
-
-    python3 bartender.py
 """
 
 
-def release_and_wait(gripper):
+def release_and_wait(gripper: InterbotixGripperXSInterface):
     """Command the gripper to release and wait until it stops moving."""
     gripper.release()  # command the gripper to release
-    while gripper.gripper_moving:  # check if the gripper is still moving
-        time.sleep(0.1)  # wait briefly before checking again
+    # while gripper.gripper_moving:  # check if the gripper is still moving
+    #     logger.info("Waiting for gripper release...")
+    #     time.sleep(0.1)  # wait briefly before checking again
 
 
-def grasp_and_wait(gripper):
+def grasp_and_wait(gripper: InterbotixGripperXSInterface):
     """Command the gripper to grasp with specified force and wait until it stops moving."""
+    gripper.set_pressure(1.0)
     gripper.grasp()  # command the gripper to grasp
     while gripper.gripper_moving:  # check if the gripper is still moving
         time.sleep(0.1)  # wait briefly before checking again
+    time.sleep(3.0)
 
 
 def forward_grasp():
@@ -42,14 +41,17 @@ def forward_grasp():
         bot.shutdown()
         sys.exit()
 
+    logger.info(f"Performing grasping!")
+
+    # release_and_wait(bot.gripper)
+
+    bot.arm.go_to_sleep_pose()
     bot.arm.go_to_home_pose()
-    print(bot.arm.get_ee_pose())
     # bot.arm.go_to_sleep_pose()
-    print(bot.arm.get_ee_pose())
     release_and_wait(bot.gripper)
 
-    bot.arm.set_single_joint_position(joint_name="shoulder", position=np.pi / 3)
-    bot.arm.set_single_joint_position(joint_name="wrist_angle", position=np.pi / 6)
+    bot.arm.set_single_joint_position(joint_name="shoulder", position=np.pi / 3.5)
+    bot.arm.set_single_joint_position(joint_name="wrist_angle", position=np.pi / 7)
     bot.gripper.grasp(0)
     time.sleep(2)
     bot.arm.go_to_home_pose()
